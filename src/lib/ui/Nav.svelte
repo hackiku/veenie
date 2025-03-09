f441de437b458ede127235f3c07c6e09b5a4afce
hackiku <ivan@pipewriter.io>
2025-03-09 12:36:00 +0100

    svelte config node 22.x

--- src/lib/ui/Nav.svelte
+++ src/lib/ui/Nav.svelte
@@ -1,15 +1,13 @@
 <!-- src/lib/ui/Nav.svelte -->
 <script lang="ts">
-  import { Play } from 'phosphor-svelte';
+  // import { Play } from 'phosphor-svelte';
   import { page } from '$app/stores';
   
   // Optional props for customization
   let { 
     showHome = true,
-    theme = 'dark'
   } = $props<{
     showHome?: boolean;
-    theme?: 'dark' | 'light';
   }>();
   
   // Determine active route
@@ -39,7 +37,8 @@
         href="/" 
         class="flex items-center mr-4 text-white font-mono" 
       >
-        <Play size={12} weight={isActive('/') ? 'fill' : 'regular'} />
+			<!-- <Play size={12} weight={isActive('/') ? 'fill' : 'regular'} /> -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#ffffff" viewBox="0 0 256 256" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-000000, #000000);"><path d="M232.4,114.49,88.32,26.35a16,16,0,0,0-16.2-.3A15.86,15.86,0,0,0,64,39.87V216.13A15.94,15.94,0,0,0,80,232a16.07,16.07,0,0,0,8.36-2.35L232.4,141.51a15.81,15.81,0,0,0,0-27ZM80,215.94V40l143.83,88Z"></path></svg>
       </a>
     {/if}
     
